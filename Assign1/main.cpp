e661a0f1c8ff711c8babbc4900e643aadfc91ad8
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-09-01 21:41:12 -0700

    Update main.cpp

--- Assign1/main.cpp
+++ Assign1/main.cpp
@@ -5,18 +5,20 @@
 #include <sys/time.h>
 #include "ReadyQueue.h"
 
+using namespace std;
+
 int main(int argc, char* argv[]) {
-	//Print basic information about the program
-	std::cout << "CS 433 Programming assignment 1" << std::endl;
-	std::cout << "Author: xxxxxx and xxxxxxx" << std::endl;
-	std::cout << "Date: xx/xx/20xx" << std::endl;
-	std::cout << "Course: CS433 (Operating Systems)" << std::endl;
-	std::cout << "Description : Program to implement a priority ready queue of processes" << std::endl;
-	std::cout << "=================================" << std::endl;
+        //Print basic information about the program
+        cout << "CS 433 Programming assignment 1" << endl;
+        cout << "Author: Thao Phung and Sophia Nguyen" << endl;
+        cout << "Date: 09/22/2021" << endl;
+        cout << "Course: CS 433 (Operating Systems)" << endl;
+        cout << "Description : Program to implement a priority ready queue of processes" << endl;
+        cout << "=================================" << endl;
 
-	// TODO: Add your code for Test 1
-	std::cout << "Performing Test 1" << std::endl;
+        // TODO: Add your code for Test 1
+        cout << "Performing Test 1" << std::endl;
 
-	// TODO: Add your code for Test 2
-	std::cout << "Performing Test 2" << std::endl;
-}  
+        // TODO: Add your code for Test 2
+        cout << "Performing Test 2" << endl;
+}
