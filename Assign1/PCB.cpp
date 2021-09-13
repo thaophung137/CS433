f74401f922b9ef384b49d0fc1467dd49bff3ae94
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-09-12 21:04:48 -0700

    Update PCB.cpp

--- Assign1/PCB.cpp
+++ Assign1/PCB.cpp
@@ -6,3 +6,34 @@ PCB::PCB(){
     priority = 0;
     state = ProcState::NEW;
 }
+
+PCB::PCB(int pcdID, int pcdPriority, ProcState pcdState) {
+  id = pcdID;
+  priority = pcdPriority;
+  state = ProcState::NEW;
+}
+
+int PCB:: getID(){
+  return id;
+}
+
+int PCB:: getPriority(){
+  return priority;
+}
+
+ProcState PCB:: getState(){
+  return state;
+}
+
+void PCB:: setID(int pcdID) {
+  id = pcdID;
+}
+
+void PCB:: setPriority(int pcdPriority) {
+  priority = pcdPriority;
+}
+
+void PCB:: setState(ProcState pcdState) {
+  state = pcdState;
+}
+
