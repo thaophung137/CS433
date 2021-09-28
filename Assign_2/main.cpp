7feb16baacedf5cdfb22f8b23842945b264dd299
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-09-27 21:45:41 -0700

    Update main.cpp

--- Assign_2/main.cpp
+++ Assign_2/main.cpp
@@ -1,3 +1,9 @@
+/********************************************************************************************
+CS 433 Programming assignment 1
+Author: Thao Phung and Sophia Nguyen
+Course: CS 433 (Operating Systems)
+*******************************************************************************************/
+
 #include <stdio.h> 
 #include <unistd.h>
 #include <iostream>
@@ -9,6 +15,13 @@ using namespace std;
 
 int main(void)
 {
+  cout << "CS 433 Programming Assignment 2" << endl;
+  cout << "Author: Thao Phung and Sophia Nguyen" << endl;
+  cout << "Date: 10/10/2021" << endl;
+  cout << "Course: CS 433 (Operating Systems)" << endl;
+  cout << "Description : Program to serve as a shell interface that accepts user commands and executes each command in a seperate process." << endl;
+  cout << "=================================" << endl;
+
   char *args[MAX_LINE/2 + 1]; /* command line arguments */
   int should_run = 1; /* flag to determine when to exit program */
   while (should_run) 
