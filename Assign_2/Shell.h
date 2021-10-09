ed04e154be17c47261dca049c47999b2b2da26e1
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-10-09 16:51:34 -0700

    Update Shell.h

--- Assign_2/Shell.h
+++ Assign_2/Shell.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <vector>
+
+using namespace std;
+
+#define MAX_LINE 80 /* The maximum length command */
 
 class Shell {
   public:
     void execCommandShell(char *args[]);
     void execCommandUser(char *args[]);
-    
+    bool ampersand = false;
+
+  private:
+    vector<string> history;
 };
