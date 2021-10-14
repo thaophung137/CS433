e604cd52133ab933a37c8a5e3f019443f5ff3076
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-10-13 18:17:25 -0700

    added new functions and variables

--- Assign_2/Shell.h
+++ Assign_2/Shell.h
@@ -7,10 +7,12 @@ using namespace std;
 
 class Shell {
   public:
-    void execCommandShell(char *args[]);
-    void execCommandUser(char *args[]);
+    void runShellCommand(char *args[]);
+    void runUserCommand(char *args[]);
     void checkRedirection(char *args[]);
+    bool checkValidCommand(char *args[]);
     bool ampersand = false;
+    int should_run = 1; //flag to determine when to exit program
 
   private:
     vector<string> history;
