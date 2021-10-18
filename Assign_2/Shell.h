a796db7f26633160f067146810b792e13c1fe94d
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-10-17 21:35:21 -0700

    Update Shell.h

--- Assign_2/Shell.h
+++ Assign_2/Shell.h
@@ -7,18 +7,18 @@ using namespace std;
 
 class Shell {
   public:
-    void runShellCommand(char *args[]);
-    void runUserCommand(char *args[]);
-    void checkRedirection(char *args[]);
-    bool checkValidCommand(char *args[]);
-    bool ampersand = false;
-    int should_run = 1; //flag to determine when to exit program
-
-    int inputFlag, outputFlag;
+    Shell();//constructor for Shell class
+    void parser(char userInput[], char *args[]);//parse user input
+    void runShellCommand(char *args[]);//execute the shell command
+    void runUserCommand(char *args[]); //execute the user command
+    void checkRedirection(char *args[]); //check for redirection in the command
+    bool checkValidCommand(char *args[]); //check if it is a valid user command 
+    int should_run; //flag to determine when to exit program
+    int inputFlag, outputFlag; 
     int inFile, outFile;
     int saveIn, saveOut;
 
   private:
-    vector<string> history;
-    
+    vector<string> history; //used to store history of commands
+    bool ampersand; //check if there is ampersand 
 };
