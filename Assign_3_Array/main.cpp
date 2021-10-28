4854f5373cf564179fa64c821be23d61592e996f
Thao Phung <thaophung137@gmail.com>
2021-10-28 12:31:44 -0700

    Update main.cpp

--- Assign_3_Array/main.cpp
+++ Assign_3_Array/main.cpp
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include <iostream>
+#include <fstream>
 
 using namespace std; 
 #define SIZE    100
@@ -15,32 +14,6 @@ int main(int argc, char *argv[])
     cout << "Description : **** " << endl;
     cout << "=================================" << endl;
 
-    FILE *in;
-    char *temp;
-    char task[SIZE];
-
-    char *name;
-    int priority;
-    int burst;
-
-    in = fopen(argv[1],"r");
-    
-    while (fgets(task,SIZE,in) != NULL) {
-        temp = strdup(task);
-        name = strsep(&temp,",");
-        priority = atoi(strsep(&temp,","));
-        burst = atoi(strsep(&temp,","));
-
-        // add the task to the scheduler's list of tasks
-        add(name,priority,burst);
-
-        free(temp);
-    }
-
-    fclose(in);
-
-    // invoke the scheduler
-    schedule();
 
     return 0;
 }
