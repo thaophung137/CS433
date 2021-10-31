fb5d9872ed54ace0e8bc4b1600a666a61e21cfa9
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-10-30 19:28:20 -0700

    Create RoundRobin.h

---
+++ Assignment3_V2/RoundRobin.h
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "Task.h"
+#include "CPU.h"
+#include <vector>
+
+using namespace std;
+
+
+class RoundRobin{
+  public:
+    RoundRobin();
+    void add(char*, int, int);
+    void schedule();
+    void displayStats();
+    
+  private:
+    vector<Task> data;
+    vector<Task> copyData;
+    vector<int>turnTime;
+    vector<int>waitTime;
+    int remainBurst;
+    int initialSize;
+    int highestPriority;
+    Task nextTask();
+    int calcTurnTime();
+    void calcWaitTime();
+    double calcAvgWait();
+    double calcAvgTurn();
+    
+    
+};
