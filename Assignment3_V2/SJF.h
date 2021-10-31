8a06d626ad39dd7ffe6105981ff519c351456bbf
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-10-30 19:25:29 -0700

    Update SJF.h

--- Assignment3_V2/SJF.h
+++ Assignment3_V2/SJF.h
@@ -12,15 +12,13 @@ class SJF{
     SJF();
     void add(char*, int, int);
     void schedule();
-    void displayStats(Task);
+    void displayStats();
     
   private:
     vector<Task> data;
     vector<Task> orderedData;
-    int doOnce;
     vector<int>turnTime;
     vector<int>waitTime;
-    int pos = 0;
     int initialSize;
     int shortestIndex;
     Task nextTask();
