3bb054f50120adf217df4da1f3479ede24cf9096
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-11-03 17:15:58 -0700

    Update main.cpp

--- Assignment3_V2/main.cpp
+++ Assignment3_V2/main.cpp
@@ -15,6 +15,7 @@
 
 #include "Task.h"
 #include "schedulers.h"
+#include "CPU.h"
 
 using namespace std; 
 
@@ -22,54 +23,67 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "CS 433 Programming assignment 3" << endl;
-    cout << "Author: Thao Phung and Sophia Nguyen" << endl;
-    cout << "Date: 10/21/2021 " << endl;
-    cout << "Course: CS433 (Operating Systems)" << endl;
-    cout << "Description : **** " << endl;
-    cout << "=================================" << endl;
+  cout << "CS 433 Programming assignment 3" << endl;
+  cout << "Author: Thao Phung and Sophia Nguyen" << endl;
+  cout << "Date: 10/21/2021 " << endl;
+  cout << "Course: CS433 (Operating Systems)" << endl;
+  cout << "Description : **** " << endl;
+  cout << "=================================" << endl;
 
-    int TQ; //Time Quantum
-    //Check that input file is provided at command line
-    if(argc < 2){
-        cerr << "Usage: " << argv[0] << " <input_file> [<time_quantum>]" << endl;
-        exit(1);
-    }
+  int TQ; //Time Quantum
+  CPU cpu1;
+  //Check that input file is provided at command line
+  if(argc < 2)
+  {
+    cerr << "Usage: " << argv[0] << " <input_file> [<time_quantum>]" << endl;
+    exit(1);
+  }
 
-    if(argc >= 3){
-        TQ = atoi(argv[2]);
-    }
+  if(argc >= 3)
+  {
+    TQ = atoi(argv[2]);
+  }
 
-    string name;
-    int priority;
-    int burst;
+  string name;
+  int priority;
+  int burst;
 
-    schedulers s;
+  schedulers s;
 
-    // open the input file
-    ifstream infile(argv[1]);
-    string line;
-    while(getline(infile, line) ) {
-        istringstream ss (line);
-        // Get the task name
-        getline(ss, name, ',');
+  // open the input file
+  ifstream infile(argv[1]);
+  string line;
+  while(getline(infile, line) ) {
+    istringstream ss (line);
+    // Get the task name
+    getline(ss, name, ',');
         
-        // Get the task priority 
-        string token;
-        getline(ss, token, ',');
-        priority = stoi(token);
+    // Get the task priority 
+    string token;
+    getline(ss, token, ',');
+    priority = stoi(token);
 
-        // Get the task burst length 
-        getline(ss, token, ',');
-        burst = stoi(token);
+    // Get the task burst length 
+    getline(ss, token, ',');
+    burst = stoi(token);
         
-        char* c = strcpy(new char[name.length() + 1], name.c_str());
-        cout << c << " " << priority << " " << burst << endl;
-        s.add(c, priority, burst);
+    char* c = strcpy(new char[name.length() + 1], name.c_str());
+    cout << c << " " << priority << " " << burst << endl;
+
+    if(argc==2)
+    {
+      s.add(c, priority, burst);
     }
 
-    //invoke the scheduler
-    s.schedule();
+    else if(argc >= 3)
+    {
+      s.add(c, priority, burst,TQ);
+    }
+  }
 
-    return 0;
+  //invoke the scheduler
+  s.schedule();
+
+  return 0;
+  
 }
