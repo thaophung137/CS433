04f51c27a9c5462098cae435d9e56c8c4cfcbb14
nguye583 <89946749+nguye583@users.noreply.github.com>
2021-12-09 13:49:58 -0800

    Add files via upload

---
+++ Assign_5_V2/pagetable.cpp
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+
+#include "pagetable.h"
+
+// TODO: Add your implementation of PageTable
+
+PageTable::PageTable(){
+    totalReferences = 0;
+    totalPageFaults = 0;
+    totalReplacements = 0;
+    timeElapsed = 0.00;
+
+} 
+PageTable::~PageTable(){
+  
+}
+
+void PageTable::display(){
+  cout << "Total memory references: " << totalReferences << endl;
+	cout << "Total page faults: " << totalPageFaults << endl;
+	cout << "Total replacements: " << totalReplacements << endl;
+  cout << "Total time elapsed: " << timeElapsed << endl;
+}
+
+void PageTable::insert(int size)
+{
+  for (int i = 0; i < size; i++) {
+		PageEntry page;
+		page.dirty = false;
+		page.valid = false;
+    page.value = 0;
+
+		page_table.push_back(page);
+	}
+}
+
+void PageTable::random(PageEntry &page)
+{
+  int randNum = rand() % page_table.size();
+	int entry = randNum;
+  
+	page_table[entry] = page;  
+}
+
+void PageTable::FIFO(PageEntry &page)
+{
+	int replace = lineCount;
+	lineCount++;
+
+	page_table[replace] = page; 
+
+}
+
+int PageTable::find(int lookFor)
+{
+  for(int i = 0; i < page_table.size(); i++)
+  {
+    if(page_table[i].value == lookFor)
+    {
+      return i;
+    }
+  }
+  return -1;
+}
---
+++ Assign_5_V2/pagetable.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <vector>
+#include<iostream>
+
+
+using namespace std;
+// Remember to add comments to your code
+
+// A page table entry
+class PageEntry
+{
+public:
+	// Physical frame number for a given page
+	int frame_num;
+	// valid bit represents whether a page is in the physical memory
+	bool valid;
+	// dirty bit represents whether a page is changed
+	bool dirty;
+  int value;
+  int last;
+	//Default Constructor
+	PageEntry(){
+		frame_num = 0;
+		valid = false;
+		dirty = false; 
+	}
+  
+  PageEntry(int valueP, bool validP, int lastP) 
+  {
+		value = valueP;
+		valid = validP;
+		last = lastP;
+	}
+};
+
+
+/**
+ * \brief A page table is like an array of page entries. The size of the page table should equal to the number of pages in logical memory
+ */
+class PageTable
+{
+  private:
+	// TODO: Add your implementation of the page table here
+	int totalReferences;
+	
+	int totalReplacements;
+	int pageNumber;
+	int frameNumber; 
+  double timeElapsed;
+  int lineCount;
+  
+
+  public: 
+  vector<PageEntry> page_table; 
+  int lineNum;
+  int totalPageFaults;
+  int pageSize;
+  int memSize;
+
+  
+	PageTable();
+	~PageTable(); 
+
+	void display();
+	void insert(int size);
+  int find(int);
+
+	void random(PageEntry &page);
+	void FIFO(PageEntry &page);
+	void LRU(PageEntry &page);
+};
